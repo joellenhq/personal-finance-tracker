34ef2ddeee2f14ebb1d0da9a40a3582ac3888c28
joellenhq <73105482+joellenhq@users.noreply.github.com>
2022-02-11 02:00:13 +0100

    Prepare data and linear regression model
    The model has very low accuracy (around 3%) possibly due to small amount of data and non-linearities

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import seaborn as sns
 import pandas as pd
+from sklearn.model_selection import train_test_split
 pd.plotting.register_matplotlib_converters()
 import matplotlib.pyplot as plt
 import numpy as np
+import datetime
+from datetime import date
+from sklearn import linear_model
+
+
+def count_linear_model(x, y):
+
+    x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.8, test_size=0.2, random_state=0)
+    linear_model1 = linear_model.LinearRegression()
+    linear_model1.fit(x_train, y_train)
+    acc = linear_model1.score(x_test, y_test)
+    print(acc)
+    print('Coefficient: \n', linear_model1.coef_)
+    print("Intercept: \n", linear_model1.intercept_)
+    return linear_model1
 
 dataset = pd.read_csv('cosmetics-spendings.csv',encoding='cp1252')
 print(dataset.head())
@@ -37,7 +53,7 @@ plt.figure('Overall spendings')
 sns.lineplot(x=unique_dates , y=month_price)
 plt.xlabel('Date of purchase')
 plt.ylabel('Expences [zł]')
-plt.show()
+#plt.show()
 
 category_price=np.zeros(len(categories))
 for i,data in enumerate(categories):
@@ -51,8 +67,45 @@ plt.title('Categories')
 sns.barplot(x=categories, y=category_price)
 plt.xlabel('Category')
 plt.ylabel('Spendings')
-plt.show()
+#plt.show()
+
+#change column date type String to type datetime
+dataset['date'] = pd.to_datetime(dataset['date'])
+print(dataset['date'])
+dataset['date'] = dataset['date'].map(datetime.datetime.toordinal)
+
+#dataset['category'] = pd.to_numeric(pd.to_numeric())
+#dataset.category = dataset.category.apply(pd.to_numeric, errors='coerce')
 
 
+dataset.drop(['name','product'],axis = 1, inplace = True)
 
+y = dataset.price
 
+dataset['category'] = dataset['category'].astype('category')
+dataset['category'] = dataset['category'].cat.codes
+print(dataset['category'])
+
+x = dataset.drop(['price'], axis = 1)
+print(dataset.head())
+
+count_linear_model(x,y)
+
+#make predictions of the amount of spendings based on a date
+y = unique_dates
+x = month_price
+
+#model = count_linear_model(x,y)
+
+while True:
+    year, month, day = 2020, 1, 1
+    today = date.today()
+    input_date = datetime.date(year, month, day)
+    while today >= input_date:
+        print('Put date of the purchase:')
+        year = input('Year: ')
+        month = input('Month: ')
+        day = input('Day: ')
+        input_date = datetime.datetime(year, month, day)
+        string_date = day+'-'+month+'-'+year
+        #predictions = model.predict(string_date)
